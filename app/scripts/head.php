e8525ef94f5613fc655513764f7884b22919ee23
Michal Ševčík <multi@tricker.cz>
2014-03-22 13:29:38 +0100

    Table "Earlier this time" is now included from script and doubled code was deleted.

--- app/scripts/head.php
+++ app/scripts/head.php
@@ -65,40 +65,9 @@
 
         <div class='col-md-3 horniDruhy'>
 
-          <div class='drivetoutodobouted'>
-          <table class='tabulkaVHlavicce'>
-            <tr class='radek'>
-              <td colspan='3'><a href='./scripts/modals/driveToutoDobou.php?je=".$_GET['je']."&amp;ja=".$_GET['ja']."' class='modal'><b>{$lang['drivetoutodobou']}</b> <img src='./images/nw.png' title='{$lang['historie']}'></a></td>
-            </tr>";
-
-            // posledni dny do pole
-            $dny2 = Array();
-            for($a = 1; $a < 6; $a++)
-            {
-              $dny2[] = date("Y-m-d H:i", mktime(date("H"), date("i"), date("s"), date("m"), date("d")-$a, date("Y")));
-            }
-  
-            // projdeme pole, pro kazdy den a podobnou dobu nalezneme teplotu a vypiseme        
-            for($a = 0; $a < count($dny2); $a++)
-            {
-  
-              $dotaz = MySQLi_query($GLOBALS["DBC"], "SELECT kdy, teplota, vlhkost
-                                    FROM tme 
-                                    WHERE kdy >= CAST('".substr($dny2[$a], 0, 14)."0' AS datetime)
-                                          AND kdy <= CAST('".substr($dny2[$a], 0, 14)."9' AS datetime)
-                                    LIMIT 1");
-              $hod = MySQLi_fetch_assoc($dotaz);
-  
-              echo "<tr>
-                      <td>".formatDnu($dny2[$a])."</td>
-                      <td><abbr title='".substr($hod['kdy'], 11, 5)."'>".jednotkaTeploty($hod['teplota'], $u, 1)."</abbr></td>";
-                      if($vlhkomer == 1){ echo "<td>".($hod['vlhkost'] != 0 ? "{$hod['vlhkost']}%" : "-")."</td>"; }
-                    echo "</tr>";
-
-            }
-
-         echo "</table>
-          </div>
+          <div class='drivetoutodobouted'>";
+            require_once dirname(__FILE__)."/ajax/drive-touto-dobou.php";
+          echo "</div>
          
         </div>
 
