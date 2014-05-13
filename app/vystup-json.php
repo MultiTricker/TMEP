79bb6a1f150e6fb7a36f680557ba93ca3a17cd44
Michal Ševčík <multi@tricker.cz>
2014-05-13 19:46:36 +0200

    Přejmenování skriptu pro výstup z JSONu od Martina Vancla aby jménem zapadl mezi ostatní výstupy + jeho drobné učesání.

---
+++ app/vystup-json.php
@@ -0,0 +1,38 @@
+<?php
+
+  /* Vystup do JSONu, autor https://github.com/tuxmartin */
+
+  //////////////////////////////////////////////////////////////////////////
+  //// VLOZENI SOUBORU
+  //////////////////////////////////////////////////////////////////////////
+
+  require "./config.php";
+  require "./scripts/db.php";
+
+  header('Expires: ' . gmdate('D, d M Y H:i:s') . '  GMT');
+  header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . '  GMT');
+  header('Content-Type: application/json; charset=utf-8');
+  header("Cache-Control: no-store, no-cache, must-revalidate");
+  header("Pragma: no-cache");
+  header("Access-Control-Allow-Origin: *");
+
+  //////////////////////////////////////////////////////////////////////////
+  //// Nacteni a naformatovani hodnoty
+  //////////////////////////////////////////////////////////////////////////
+
+  $q = MySQLi_query($GLOBALS["DBC"], "SELECT teplota, vlhkost, kdy
+                                      FROM tme
+                                      ORDER BY id DESC
+                                      LIMIT 1");
+
+  echo mysqli_error($GLOBALS["DBC"]);
+
+  $posledni = MySQLi_fetch_assoc($q);
+
+?>{
+   "teplota": <?php echo $posledni["teplota"] ?>,
+   "vlhkost": <?php echo ($vlhkomer == 1 ? $posledni["vlhkost"] : "null") ?>,
+   "cas": "<?php echo $posledni["kdy"] ?>",
+   "umisteni": "<?php echo $umisteni; ?>",
+   "popis": "Teplomer"
+}
