a89e01843ed107a4136826a1c0537f3e501f3a07
Michal Ševčík <multi@tricker.cz>
2014-05-13 20:21:47 +0200

    Čitelná chybová hláška v případě problému se spojením na databázi či jejím výběrem. Přispěl Petr Domorázek - díky!

--- app/scripts/db.php
+++ app/scripts/db.php
@@ -1,9 +1,13 @@
 <?php
 
 if(!($GLOBALS["DBC"] = MySQLi_connect($dbServer, $dbUzivatel, $dbHeslo)))
-  echo "Nepodarilo se spojit s databazovym serverem a prihlasit se. Prosim, zkontrolujte nastaveni.<br>
-        Unable to connect to database server and log in. Please, check out your settings.";
+  die ("<div style=\"float: center; margin: auto; background-color:white; border: solid red 5px; padding: 5px; font-weight: bold;\">
+        Nepodarilo se spojit s databazovym serverem a prihlasit se. Prosim, zkontrolujte nastaveni.<br>
+        Unable to connect to database server and log in. Please, check out your settings.
+        </div>");
 
 if(!((bool)mysqli_query($GLOBALS["DBC"], "USE {$dbDb}")))
-  echo "Chyba ve vybrani databaze \"{$dbDb}\". Prosim, zkontrolujte nastaveni.<br>
-        Unable to select database \"{$dbDb}\". Please, check out your settings.";
+  die ("<div style=\"float: center; margin: auto; background-color:white; border: solid red 5px; padding: 5px; font-weight: bold;\">
+        Chyba ve vybrani databaze \"{$dbDb}\". Prosim, zkontrolujte nastaveni.<br>
+        Unable to select database \"{$dbDb}\". Please, check out your settings.
+        </div>");
