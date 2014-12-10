f426f278dbf03536c731b885d350dbf551bb1981
Michal Ševčík <multi@tricker.cz>
2014-12-10 21:18:06 +0100

    FIX pripojeni k DB, nazev vlozen do backticku.

--- app/scripts/db.php
+++ app/scripts/db.php
@@ -6,7 +6,7 @@ if(!($GLOBALS["DBC"] = MySQLi_connect($dbServer, $dbUzivatel, $dbHeslo)))
         Unable to connect to database server and log in. Please, check out your settings.
         </div>");
 
-if(!((bool)mysqli_query($GLOBALS["DBC"], "USE {$dbDb}")))
+if(!((bool)mysqli_query($GLOBALS["DBC"], "USE `{$dbDb}`")))
   die ("<div style=\"float: center; margin: auto; background-color:white; border: solid red 5px; padding: 5px; font-weight: bold;\">
         Chyba ve vybrani databaze \"{$dbDb}\". Prosim, zkontrolujte nastaveni.<br>
         Unable to select database \"{$dbDb}\". Please, check out your settings.
