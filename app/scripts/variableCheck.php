2b2229003535e8296b1fa34e7a801216c89071b6
Michal Ševčík <multi@tricker.cz>
2016-12-20 20:09:35 +0100

    Fixed XSS vulnerability.

--- app/scripts/variableCheck.php
+++ app/scripts/variableCheck.php
@@ -1,49 +1,55 @@
 <?php
 
-  // Prvotni INIT
-  if(!isset($_GET['je'])){ $_GET['je'] = $l; }
-  if(!isset($_GET['ja'])){ $_GET['ja'] = $u; }
+// Prvotni INIT
+if(!isset($_GET['je'])){ $_GET['je'] = $l; }
+if(!isset($_GET['ja'])){ $_GET['ja'] = $u; }
 
-  $jazyky = array("cz" => "cz",
-                  "sk" => "sk",
-                  "en" => "en",
-                  "de" => "de",
-                  "ru" => "ru",
-                  "pl" => "pl",
-                  "fr" => "fr",
-                  "fi" => "fi",
-                  "sv" => "sv",
-                  "pt" => "pt");
+$jazyky = array("cz" => "cz",
+  "sk" => "sk",
+  "en" => "en",
+  "de" => "de",
+  "ru" => "ru",
+  "pl" => "pl",
+  "fr" => "fr",
+  "fi" => "fi",
+  "sv" => "sv");
 
-  $jednotky = Array("C" => "Celsius",
-                    "F" => "Fahrenheit",
-                    "K" => "Kelvin",
-                    "R" => "Rankine",
-                    "D" => "Delisle",
-                    "N" => "Newton",
-                    "Re" => "Reaumur",
-                    "Ro" => "Romer");
+$jednotky = Array("C" => "Celsius",
+  "F" => "Fahrenheit",
+  "K" => "Kelvin",
+  "R" => "Rankine",
+  "D" => "Delisle",
+  "N" => "Newton",
+  "Re" => "Reaumur",
+  "Ro" => "Romer");
 
-  // Davame moznost zobrazit nastaveni?
-  if($zobrazitNastaveni == 1)
+// Davame moznost zobrazit nastaveni?
+if($zobrazitNastaveni == 1)
+{
+  // jazyk
+  if(isset($_GET['ja']) AND isset($jazyky[$_GET['ja']]))
   {
-
-    // jazyk
-    if(isset($_GET['ja']) AND array_key_exists($_GET['ja'], $jazyky))
-    {
-      $l = $_GET['ja'];
-    }
-
-    require_once dirname(__FILE__)."/language/".$l.".php";       // skript s jazykovou mutaci
-
-    // jednotka
-    if(isset($_GET['je']) AND array_key_exists($_GET['je'], $jednotky))
-    {
-      $u = $_GET['je'];
-    }
-
+    $l = $jazyky[$_GET['ja']];
   }
   else
   {
-    require_once dirname(__FILE__)."/language/".$l.".php";       // skript s jazykovou mutaci
+    $_GET['ja'] = $l;
   }
+
+  require_once dirname(__FILE__)."/language/".$l.".php";       // skript s jazykovou mutaci
+
+  // jednotka
+  if(isset($_GET['je']) AND isset($jednotky[$_GET['je']]))
+  {
+    $u = $_GET['je'];
+  }
+  else
+  {
+    $_GET['je'] = $u;
+  }
+
+}
+else
+{
+  require_once dirname(__FILE__)."/language/".$l.".php";       // skript s jazykovou mutaci
+}
